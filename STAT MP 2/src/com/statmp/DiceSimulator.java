d80eca76d805be5cd15d6998b94217e7c9ce9af4
Jj <jeruel_trinidad@dlsu.edu.ph>
2016-04-02 23:02:19 +0800

    Update DiceSimulator.java

--- STAT MP 2/src/com/statmp/DiceSimulator.java
+++ STAT MP 2/src/com/statmp/DiceSimulator.java
@@ -5,12 +5,34 @@ import java.util.Scanner;
 public class DiceSimulator {
 	
 	private int nDice;
+	private int goalRoll;
+	private int correctGuessCount;
 	private long trials;
 	private Die[] dice;
 	
-	public DiceSimulator() {
+	public DiceSimulator() 
+	{
+		correctGuessCount=0;
+		do
+		{
 		nDice = askInput("How many dice would you like to throw?");
+		}while(nDice<1 || nDice>5);
+		
+		dice = new Die[nDice];
+		for(int i = 0; i < dice.length; i++) 
+		{
+			dice[i] = new Die();
+		}
+		
+		do
+		{
 		trials = askInput("How many trials?");
+		}while(trials<1 || trials>100000);
+		
+		do
+		{
+		goalRoll = askInput("What's your goal roll?");
+		}while(goalRoll<dice.length || goalRoll>dice.length*6);
 		start();
 	}
 	
@@ -20,19 +42,35 @@ public class DiceSimulator {
 		return sc.nextInt();
 	}
 	
-	public void start() {
-		dice = new Die[nDice];
-		int[] results = new int[nDice];
-		for(int i = 0; i < dice.length; i++) {
-			dice[i] = new Die();
+	private int getRollTotal(int[] results)
+	{
+		int total=0;
+		for(int i=0; i<results.length; i++)
+		{
+			total += results[i];
 		}
-		
-		for(int i = 0; i < trials; i++) {
-			for(int j = 0; j < dice.length; j++) {
+		return total;
+	}
+	
+	private void countCorrectGuesses(int[] results)
+	{
+		if(getRollTotal(results)==goalRoll)
+			correctGuessCount++;
+	}
+	
+	public void start() 
+	{	
+		int[] results = new int[nDice];
+		for(int i = 0; i < trials; i++) 
+		{
+			for(int j = 0; j < dice.length; j++) 
+			{
 				results[j] = dice[j].hurl();
 			}
+			countCorrectGuesses(results);
 			Logger.getInstance().logTrial(i+1, results);
 		}
+		Logger.getInstance().logCorrectGuessCount(correctGuessCount);
 		
 		Logger.getInstance().generateLog();
 	}
