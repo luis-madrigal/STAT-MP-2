8a30b8b3257b8e9c511690cea49b777175344c39
Luis Madrigal <lmadrigal.111@gmail.com>
2016-04-10 18:19:31 +0800

    Update Histogram.java

--- STAT MP 2/src/com/statmp/Histogram.java
+++ STAT MP 2/src/com/statmp/Histogram.java
@@ -2,9 +2,9 @@ package com.histograms;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import javax.swing.JPanel;
+import javax.swing.ToolTipManager;
 
 public class Histogram extends JPanel{
 	
@@ -19,7 +19,6 @@ public class Histogram extends JPanel{
 	
 	
 	private Break[] breaks;
-	private double[] counts;
 	
 	private int noOfDice;
 	private int height;
@@ -43,10 +42,11 @@ public class Histogram extends JPanel{
 		this.countFreq();
 	}
 	
-	public Histogram(int noOfDice, int height, int width) {
+	public Histogram(int noOfDice, int height, int width, int goalRoll) {
 		this.noOfDice = noOfDice;
 		this.height = height;
 		this.width = width;
+		this.goalRoll = goalRoll;
 		
 		this.breaks = new Break[(noOfDice*6)-noOfDice+1];
 		this.breakHeights = new int[breaks.length];
@@ -63,6 +63,7 @@ public class Histogram extends JPanel{
 	}
 	
 	private void setBreaks() {
+		ToolTipManager.sharedInstance().setInitialDelay(5);
 		int maxPoss = noOfDice*6;
 		int counter = noOfDice;
 		int i = 0;
@@ -122,9 +123,7 @@ public class Histogram extends JPanel{
 		int possCombinations = (int) Math.pow(6.0, (double)noOfDice);
 		for(int i = 0; i < breaks.length; i++) {
 			breaks[i].setProb(possCombinations);
-			System.out.print(breaks[i].getFreq()+",");
 		}
-		System.out.println();
 		this.scaleGraph(possCombinations);
 	}
 	
@@ -148,11 +147,14 @@ public class Histogram extends JPanel{
 					breakHeights[i] = 0;
 					break;
 				}
-				else if(((double)j/height) > freqPerc[i]) {
+				else if(((double)j/height) >= freqPerc[i]) {
 					breakHeights[i] = j;
 					break;
 				}				
 			}
+		}
+		if(!this.checkDupBars().isEmpty()) {
+			this.removeDupBars(N);
 		}
 		int maxBHeight = 0;
 		for(int i = 0; i < breakHeights.length; i++) {
@@ -165,52 +167,82 @@ public class Histogram extends JPanel{
 		for(int i = 0; i < breakHeights.length; i++) {
 			breakHeights[i] *= scaleMult;
 		}
-//		System.out.println();
-//		ArrayList<Break> dupVals = new ArrayList<Break>();
-//		for(int i = 0; i < breakHeights.length; i++) {
-//			for(int j = 0; j < breakHeights.length; j++) {
-//				if(breakHeights[i] == breakHeights[j] && i != j && breaks[i].getFreq() != breaks[j].getFreq()) {
-//					dupVals.add(breaks[i]);
-//					break;
-//				}
-//			}
-//		}
-//		int n = dupVals.size();
-//		int k;
-//		Break temp;
-//		for(int i = n; i >= 0; i--) {
-//			for(int j = 0; j < n -1 ; j++) {
-//				k = j+1;
-//				if(dupVals.get(j).getFreq() > dupVals.get(k).getFreq()) {
-//					temp = dupVals.get(j);
-//					dupVals.set(j, dupVals.get(k));
-//					dupVals.set(k, temp);
-//				}
-//			}
-//		}
-//		int add = 1;
-//		for(int i = 0; i < dupVals.size(); i++) {
-//			for(int j = 0; j < breaks.length; j++) {
-//				if(breaks[j].equals(dupVals.get(i)) && i != j) {
-//					System.out.println("SAME: "+dupVals.get(i).getFreq());
-//					breaks[j] = dupVals.get(i);
-//					breakHeights[j]+= add;		
-//					add++;
-//				}
-//			}
-//		}
-//		System.out.println();
+	}
+	
+	private ArrayList<Break> checkDupBars() {
+		ArrayList<Break> dupVals = new ArrayList<Break>();
+		for(int i = 0; i < breakHeights.length; i++) {
+			for(int j = 0; j < breakHeights.length; j++) {
+				if(breakHeights[i] == breakHeights[j] && i != j && breaks[i].getFreq() != breaks[j].getFreq()) {
+					dupVals.add(breaks[i]);
+				}
+			}
+		}
+		return dupVals;
+	}
+	
+	private void removeDupBars(int N) {
+		double[] freqPerc = new double[breaks.length];
+		int prevSize = -1;
+		for(int i = 0; i < breaks.length/2; i++) {
+			freqPerc[i] = ((double)breaks[i].getFreq()/N);
+			for(int j = 1; j <= height; j++) {
+				if(freqPerc[i] == 0) {
+					breakHeights[i] = 0;
+					break;
+				}
+				else if(((double)j/height) >= freqPerc[i]) {
+					breakHeights[i] = j;
+					if(prevSize == j) {
+						breakHeights[i]++;
+						prevSize = j+1;
+					}
+					else
+						prevSize = j;
+					break;
+				}				
+			}
+		}
+		prevSize = -1;
+		for(int i = breaks.length-1; i >= breaks.length/2; i--) {
+			freqPerc[i] = ((double)breaks[i].getFreq()/N);
+			for(int j = 1; j <= height; j++) {
+				if(freqPerc[i] == 0) {
+					breakHeights[i] = 0;
+					break;
+				}
+				else if(((double)j/height) >= freqPerc[i]) {
+					breakHeights[i] = j;
+					if(prevSize == j) {
+						breakHeights[i]++;
+						prevSize = j+1;
+					}
+					else
+						prevSize = j;
+					break;
+				}				
+			}
+		}
+		int x = breaks.length/2;
+		int y = breaks.length/2 - 1;
+		if(breaks[x].getFreq() != breaks[y].getFreq() && breakHeights[x] == breakHeights[y]) {
+			if(breaks[x].getFreq() > breaks[y].getFreq())
+				breakHeights[x]++;
+			else
+				breakHeights[y]++;
+		}
 	}
 	
 	public void graph(int type) {
 		this.scaleGraph(results.length);
+
 		for(int i = 0; i < breaks.length; i++) {
 			if(type == Histogram.PROB_HIST)
 				breaks[i].setProb(results.length);
 			if(breaks[i].getNum() == goalRoll)
 				breaks[i].initLabel(breakWidths[i], breakHeights[i], interval, height, Color.red, type);
 			else if(breaks[i].getFreq() == 0)
-				breaks[i].initEmptyLabel(breakWidths[i], 100, interval, height);
+				breaks[i].initEmptyLabel(breakWidths[i], 100, interval, height, type);
 			else
 				breaks[i].initLabel(breakWidths[i], breakHeights[i], interval, height, Color.darkGray, type);
 			this.add(breaks[i]);
@@ -219,12 +251,15 @@ public class Histogram extends JPanel{
 	}
 	
 	public void graphIdeal() {
-		
 		for(int i = 0; i < breaks.length; i++) {
-			breaks[i].initLabel(breakWidths[i], breakHeights[i], interval, height, Color.darkGray, Histogram.IDEAL_HIST);
+			if(breaks[i].getNum() == goalRoll)
+				breaks[i].initLabel(breakWidths[i], breakHeights[i], interval, height, Color.red, Histogram.IDEAL_HIST);
+			else
+				breaks[i].initLabel(breakWidths[i], breakHeights[i], interval, height, Color.darkGray, Histogram.IDEAL_HIST);
 			this.add(breaks[i]);
 		}
 		this.repaint();
 	}
 
 }
+
